dd1541286832a00b6ef7551d46047b9c49eea4e3
Juan Alberto Chamorro <jchamorro@unicef.es>
2019-05-15 19:01:42 +0200

    Update gmap.js

--- almansa/js/gmap.js
+++ almansa/js/gmap.js
@@ -25,7 +25,7 @@ function initMap() {
       new google.maps.LatLng(38.8041, -0.9576));
 
   // The photograph is courtesy of the U.S. Geological Survey.
-  var srcImage = 'https://jotha.github.io/almansa/images/1-1.jpg';
+  var srcImage = 'https://jotha.github.io/almansa/images/1-1.png';
 
   // The custom USGSOverlay object contains the USGS image,
   // the bounds of the image, and a reference to the map.
